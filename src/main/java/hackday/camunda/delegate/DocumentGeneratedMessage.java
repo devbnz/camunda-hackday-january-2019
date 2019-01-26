72b6e63470ecd1c17a6361ac318a7eec2a4bfa15
Louis Iskandar <louis.iskandar@medici-living.de>
2019-01-26 16:49:24 +0100

    set message.

--- src/main/java/hackday/camunda/delegate/DocumentGeneratedMessage.java
+++ src/main/java/hackday/camunda/delegate/DocumentGeneratedMessage.java
@@ -3,7 +3,7 @@ package hackday.camunda.delegate;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-public class DocumentGeneratedMessageDelegate implements JavaDelegate {
+public class DocumentGeneratedMessage implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         delegateExecution.setVariable("action","I Check my Delegate my Self");
